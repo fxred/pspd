24dc451407b7855ea559368b465473ab2df2f8b8
GeovaneSFT <galefreitas@gmail.com>
2025-10-06 22:25:00 +0000

    style: altera visual do client

--- grpc/wasm_game_client/www/index.js
+++ grpc/wasm_game_client/www/index.js
@@ -1,10 +1,19 @@
-import init from './pkg/wasm_game_client.js';
+import init, { App } from './pkg/wasm_game_client.js';
 
 async function run() {
   try {
     await init();
+
+    const apiPort = '8082'; 
+    const apiBaseUrl = `http://${window.location.hostname}:${apiPort}`;
+
+    console.log(`API URL: ${apiBaseUrl}`);
+
+    const app = new App(apiBaseUrl);
+    await app.run();
+
   } catch (error) {
-    console.error("Erro fatal ao inicializar o módulo WebAssembly:", error);
+    console.error("Erro fatal ao inicializar ou executar a aplicação:", error);
   }
 }
 
