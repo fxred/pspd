1db0da2ae23dc9ef48d6b72cd66041c058242511
renannOgomes <renannpop11@gmail.com>
2025-10-05 21:41:32 -0300

    add unary example

---
+++ grpc_examples/simple_unary_go/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	context "context"
+	log "log"
+	os "os"
+	"google.golang.org/grpc"
+	pb "simple_unary_go/helloworld"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := "World"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
---
+++ grpc_examples/simple_unary_go/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	context "context"
+	log "log"
+	net "net"
+
+	"google.golang.org/grpc"
+	pb "simple_unary_go/helloworld"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received request from client with name: %s", in.Name)
+	return &pb.HelloReply{Message: "Hello, " + in.Name + "!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Println("gRPC server listening on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
